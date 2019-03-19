d8ebb5d1da4408205fa18c5301d1f17b7755b278
Paul Wu <ahurpdurp@Pauls-MacBook-Pro.local>
2019-03-19 15:39:30 -0500

    adding video page

---
+++ favorites.js
@@ -0,0 +1,58 @@
+let marvelCharacters = document.getElementById('marvelCharacters')
+let textSubmit = document.getElementById('textSubmit')
+heroArray = [] 
+
+function arrayHeroes() {
+    //ideally get the heroarray from the firebase
+    heroArray.forEach(item => {
+        displayData(item, heroArray.indexOf(item))
+    })
+}
+
+
+function createComicDetailsUniqueId(itemIndex) {
+    return `${itemIndex}latestComicsDiv`
+}
+
+function displayData(item, itemIndex){
+    fetch("https://gateway.marvel.com/v1/public/characters?name=" + item.replace(" ","%20") + "&ts=1&apikey=d11ae31dc048dbd19178f875e7dc3ddf&hash=1b14ced7bbcdb443b4ed46455253af59")
+    .then(response => response.json())
+    .then(data => {
+        marvelCharacters.innerHTML = marvelCharacters.innerHTML + 
+            `<li><h1>${data.data.results[0].name}</h1>
+            <p>${data.data.results[0].description}</p>
+            <img src = ${data.data.results[0].thumbnail.path}/portrait_xlarge.jpg>
+            <a href = '${data.data.results[0].urls[1].url}'>Read More</a>
+            <h3>Recent Comics that ${data.data.results[0].name}'s been in: </h3>
+            <div id="${createComicDetailsUniqueId(itemIndex)}"></div></li>
+            `
+            displayDetails(data.data.results[0].id,itemIndex)
+        }
+        )
+    }
+
+function displayDetails(charId,itemIndex){
+    fetch('https://gateway.marvel.com/v1/public/characters/' + charId + '/series?ts=1&orderBy=-startYear&apikey=d11ae31dc048dbd19178f875e7dc3ddf&hash=1b14ced7bbcdb443b4ed46455253af59')
+    .then(response => response.json())
+    .then(response2 => {
+
+        let latestComicsDiv = document.getElementById(`${itemIndex}latestComicsDiv`)
+        let content = []
+        content.push(`<ul id = 'comicSeriesListUL'>Recent Comic Series lol`)
+        for(let i = 0; i < 11; i ++){
+            content.push(`<li><a href = '${response2.data.results[i].urls[0].url}'>${response2.data.results[i].title}</a></li>`)
+            if(response2.data.results[i].thumbnail.path != 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available'){
+            content.push(`<img src = ${response2.data.results[i].thumbnail.path}/portrait_xlarge.jpg>`)}
+            
+        }
+        content.push(`</ul>`)
+        latestComicsDiv.innerHTML = content.join('')
+            
+    })
+}
+
+textSubmit.addEventListener('click', () => {
+    let textField = document.getElementById('textField')
+    heroArray.push(textField.value)
+    displayData(textField.value,heroArray.indexOf(textField.value))
+})
---
+++ videoSources.js
@@ -0,0 +1,47 @@
+
+
+function returnMovieURLS(){
+    let URLS = [
+        {"movie": 'Spider-Man: Far From Home (2019)',"URL": "https://www.youtube.com/watch?v=DYYtuKyMtY8", "movieURL":"https://www.marvel.com/"},
+        {"movie": 'Avengers: End Game (2019)',"URL": "https://www.youtube.com/watch?v=hA6hldpSTF8", "movieURL":"https://www.marvel.com/"},
+        {"movie": 'Captain Marvel (2019)',"URL": "https://www.youtube.com/watch?v=Z1BCujX3pw8", "movieURL":"https://www.marvel.com/"},
+        {"movie": 'Avengers: Infinity War (2018)',"URL": "https://www.youtube.com/watch?v=6ZfuNTqbHE8", "movieURL":"http://vexmovies.org/infinity-war-avengers"},
+        {"movie": 'Ant-Man and the Wasp (2018)',"URL": "https://www.youtube.com/watch?v=UUkn-enk2RU", "movieURL":"http://vexmovies.org/ant-man-and-the-wasp"},
+        {"movie": 'Black Panther (2018)',"URL": "https://www.youtube.com/watch?v=xjDjIWPwcPU", "movieURL":"http://www4.putlocker.fm/watch-77491-black-panther-2018-putlocker.html"},
+        {"movie": 'Thor: Ragnarok (2017)',"URL": "https://www.youtube.com/watch?v=ue80QwXMRHg", "movieURL":"http://vexmovies.org/thor"},
+        {"movie": 'Spider-Man: Homecoming (2017)',"URL": "https://www.youtube.com/watch?v=n9DwoQ7HWvI", "movieURL":"http://vexmovies.org/spider-man-homecoming"},
+        {"movie": 'Guardians of the Galaxy Vol. 2 (2017)',"URL": "https://www.youtube.com/watch?v=dW1BIid8Osg", "movieURL":"http://vexmovies.org/guardians-galaxy-part-2"},
+        {"movie": 'Doctor Strange (2016)',"URL": "https://www.youtube.com/watch?v=HSzx-zryEgM", "movieURL":"http://vexmovies.org/doctor-strange"},
+        {"movie": 'Captain America: Civil War (2016)',"URL": "https://www.youtube.com/watch?v=dKrVegVI0Us", "movieURL":"http://vexmovies.org/captain-america-civil-war"},
+        {"movie": 'Ant-Man (2015)',"URL": "https://www.youtube.com/watch?v=tmeOjFno6Do", "movieURL":"http://vexmovies.org/ant-man"},
+        {"movie": 'Avengers: Age of Ultron (2015)',"URL": "https://www.youtube.com/watch?v=JAUoeqvedMo", "movieURL":"http://vexmovies.org/avengers-age-ultron"},
+        {"movie": 'Guardians of the Galaxy (2014)',"URL": "https://www.youtube.com/watch?v=d96cjJhvlMA", "movieURL":"http://vexmovies.org/guardians-of-the-galaxy"},
+        {"movie": 'Captain America: The Winter Soldier (2014)',"URL": "https://www.youtube.com/watch?v=7SlILk2WMTI", "movieURL":"http://vexmovies.org/captain-america-winter-soldier"},
+        {"movie": 'Thor: The Dark World (2013)',"URL": "https://www.youtube.com/watch?v=npvJ9FTgZbM", "movieURL":"http://vexmovies.org/thor"},
+        {"movie": 'Iron Man 3 (2013)',"URL": "https://www.youtube.com/watch?v=oYSD2VQagc4", "movieURL":"http://vexmovies.org/iron-man-3"},
+        {"movie": 'The Avengers (2012)',"URL": "https://www.youtube.com/watch?v=eOrNdBpGMv8", "movieURL":"http://vexmovies.org/the-avengers"},
+        {"movie": 'Captain America: The First Avenger (2011)',"URL": "https://www.youtube.com/watch?v=W4DlMggBPvc", "movieURL":"http://vexmovies.org/captain-america-first-avenger"},
+        {"movie": 'Thor (2011)',"URL": "https://www.youtube.com/watch?v=vJUDu40Z_RY", "movieURL":"http://vexmovies.org/thor"},
+        {"movie": 'Iron Man 2 (2010)',"URL": "https://www.youtube.com/watch?v=wKtcmiifycU", "movieURL": "http://vexmovies.org/iron-man-2"},
+        {"movie": 'The Incredible Hulk (2008)',"URL": "https://www.youtube.com/watch?v=xbqNb2PFKKA", "movieURL": "http://vexmovies.org/the-incredible-hulk"},
+        {"movie": 'Iron Man (2008)',"URL": "https://www.youtube.com/watch?v=8hYlB38asDY", "movieURL":"http://vexmovies.org/iron-man"}
+    ]
+    return URLS
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
