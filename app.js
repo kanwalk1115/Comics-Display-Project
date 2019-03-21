368c102e2d5358b93a4be9ac1a031f6271e49ceb
Bannon Smith <bannonsmith@Bannons-MBP.localdomain>
2019-03-21 14:34:25 -0500

    fixedCharactersPage

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 let marvelCharacters = document.getElementById('marvelCharacters')
 
+document.body.onload = function() {
+  arrayHeroes() 
+}
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
 
 function arrayHeroes() {
     heroArray = ['iron man','captain america','thor','black widow', 'black panther','spider-man']
